66a326efb62dbc6fb9b3120c340e6c6ef91d82c4
unknown <enricoamato997@gmail.com>
2019-01-22 12:27:17 +0100

    add day concept

---
+++ src/DaySection.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const DaySection = () => {
+  return(
+
+    <div className="card">
+      <span>Wed</span><br />
+      <i className="wi wi-day-sunny"></i> <br /><br />
+      <span className="temp">20°</span><span className="temp">25°</span>
+    </div>
+
+  )
+}
+
+export default DaySection
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return(
+    <header>
+      <h1>Weather App</h1>
+      <h3>Write down your location</h3>
+      <label>Location: </label><input type="text" />
+
+    </header>
+  )
+}
+
+export default Header
