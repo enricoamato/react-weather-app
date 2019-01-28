6d4ece71afda3511919b6f6ebe42d514e4f3b572
unknown <enricoamato997@gmail.com>
2019-01-28 13:18:46 +0100

    add conditional rendering & associative array

--- src/DayContent.js
+++ src/DayContent.js
@@ -2,24 +2,39 @@ import React from 'react'
 
 const DayContent = (props) => {
 
+  const imgArray = {
+    "Clouds": require('./imgs/icons/clouds.svg'),
+    "Drop": require('./imgs/icons/drop.svg'),
+    "Moon": require('./imgs/icons/moon.svg'),
+    "Snowflake": require('./imgs/icons/snowflake.svg'),
+    "Clear": require('./imgs/icons/sunny.svg'),
+    "Fog": require('./imgs/icons/fog.svg')
+    }
+
+
+
   return(
-
     <div className="content">
-
       <div className="location">{props.city} {props.country}</div>
 
       <div>
         <span className="temp">{props.temperature}°</span> <br/>
         <span className="description">{props.description}</span> <br/><br/>
-        <img alt="sun" src={require('./weather_icons/sun.svg')}/> <br/>
+        <img alt={props.description} src={imgArray[`${props.description}`]} /> <br/>
       </div>
 
       <div className="date">
-        <span>{props.day} 27/01/2019</span>
+        <span>{props.day}</span>
       </div>
-
     </div>
   )
 }
 
 export default DayContent
+
+
+// <img alt="sun" src={imgArray["drop"]} /> <br/>
+// <img alt="sun" src={imgArray["moon"]} /> <br/>
+// <img alt="sun" src={imgArray["snowflake"]} /> <br/>
+// <img alt="sun" src={imgArray["sunny"]} /> <br/>
+// <img alt="sun" src={imgArray["fog"]} /> <br/>
